60394e156072a2f08980b506870ec5ab1bdb0ee8
MatejB <matejbaco@gmail.com>
2016-01-31 19:07:16 +0100

    cleanup

--- main.go
+++ main.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"sync"
-	"time"
-)
+import "net/http"
 
 // main is the function that will be called when program starts, when main function exists program exists
 func main() {
@@ -31,49 +19,14 @@ func main() {
 	// https://golang.org/pkg/net/http/#ListenAndServe
 	// https://golang.org/pkg/sync/#WaitGroup
 
-	params := parseFlags()
-
-	cssPaths, err := list(params.list)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = merge(cssPaths, params.out)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var wg sync.WaitGroup
-
-	if params.watch {
-		wg.Add(1)
-		go func() {
-			err := watch(cssPaths, params.out)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
-	}
-
-	if params.serve != 0 {
-		wg.Add(1)
-		go func() {
-			http.Handle("/", MergedFile(params.out))
-
-			fmt.Printf("Serve mode: started on port %d\n", params.serve)
-			log.Fatal(http.ListenAndServe(":"+strconv.Itoa(params.serve), nil))
-		}()
-	}
-
-	wg.Wait()
 }
 
 // cliParams is structure contains cli params flags
 type cliParams struct {
-	list  string // phase 1
-	out   string // phase 1
-	watch bool   // phase 2
-	serve int    // phase 3
+	list string // phase 1
+	out  string // phase 1
+	// watch bool   // phase 2
+	// serve int    // phase 3
 }
 
 // parseFlags will parse cli program arguments into internal structure for later use
@@ -82,23 +35,14 @@ func parseFlags() (params cliParams) {
 	// Phase 1 help:
 	// https://golang.org/pkg/flag/#StringVar
 
-	flag.StringVar(&params.list, "list", "", "Path to dir containg css files")
-	flag.StringVar(&params.out, "out", "", "Filename of destination css file")
-
 	// Phase 2 help:
 	// https://golang.org/pkg/flag/#BoolVar
-
-	flag.BoolVar(&params.watch, "watch", false, "Enables watch mode that automatically rebuilds destination css file if any of source css files changes")
 
 	// Phase 3 help:
 	// https://golang.org/pkg/flag/#IntVar
 
-	flag.IntVar(&params.serve, "serve", 0, "Enables serve mode on provided port that will serve merged css file")
-
 	// help:
 	// https://golang.org/pkg/flag/#Parse
-
-	flag.Parse()
 
 	return
 }
@@ -109,16 +53,6 @@ func list(listFile string) (cssFilePaths []string, err error) {
 	// help:
 	// https://golang.org/pkg/io/ioutil/#ReadFile
 	// https://golang.org/pkg/encoding/json/#Unmarshal
-
-	content, err := ioutil.ReadFile(listFile)
-	if err != nil {
-		return cssFilePaths, err
-	}
-
-	err = json.Unmarshal(content, &cssFilePaths)
-	if err != nil {
-		return cssFilePaths, err
-	}
 
 	return
 }
@@ -132,27 +66,6 @@ func merge(cssFilePaths []string, mergedFile string) (err error) {
 	// https://golang.org/pkg/os/#Open
 	// https://golang.org/pkg/io/#Copy
 
-	out, err := os.Create(mergedFile)
-	if err != nil {
-		return
-	}
-	defer out.Close()
-
-	for _, path := range cssFilePaths {
-		in, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-
-		_, err = io.Copy(out, in)
-		if err != nil {
-			in.Close()
-			return err
-		}
-
-		in.Close()
-	}
-
 	return nil
 }
 
@@ -161,70 +74,7 @@ func watch(cssFilePaths []string, mergedFile string) (err error) {
 
 	// help: https://golang.org/pkg/os/#Stat
 
-	fmt.Println("Watch mode: started")
-
-	rebuildCh := make(chan bool)
-	errCh := make(chan error)
-	cleanupCh := make(chan struct{})
-
-	watchSingleFile := func(path string, rebuildCh chan bool, errCh chan error, cleanupCh chan struct{}) {
-		initStat, err := os.Stat(path)
-		if err != nil {
-			select {
-			case errCh <- err:
-				return
-			case <-cleanupCh:
-				return
-			}
-		}
-
-		fmt.Printf("Watch mode: added %q to watch list\n", path)
-
-		for {
-			stat, err := os.Stat(path)
-			if err != nil {
-				select {
-				case errCh <- err:
-					return
-				case <-cleanupCh:
-					return
-				}
-			}
-
-			if stat.Size() != initStat.Size() || stat.ModTime() != initStat.ModTime() {
-				select {
-				case rebuildCh <- true:
-					initStat = stat
-				case <-cleanupCh:
-					return
-				}
-			}
-
-			time.Sleep(50 * time.Millisecond)
-		}
-	}
-
-	for _, path := range cssFilePaths {
-		go watchSingleFile(path, rebuildCh, errCh, cleanupCh)
-	}
-
-	for {
-		select {
-
-		case <-rebuildCh:
-			err := merge(cssFilePaths, mergedFile)
-			if err != nil {
-				close(cleanupCh)
-				return err
-			}
-			fmt.Printf("Watch mode: rebuilded %q\n", mergedFile)
-
-		case err := <-errCh:
-			close(cleanupCh)
-			return err
-		}
-	}
-
+	return
 }
 
 // MergedFile is a custom type representing merged css file
@@ -239,20 +89,4 @@ func (mf MergedFile) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	// https://golang.org/pkg/net/http/#ServeContent
 	// https://golang.org/pkg/net/http/#Error
 
-	path := string(mf)
-
-	stat, err := os.Stat(path)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	out, err := os.Open(path)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer out.Close()
-
-	http.ServeContent(w, req, stat.Name(), stat.ModTime(), out)
 }
